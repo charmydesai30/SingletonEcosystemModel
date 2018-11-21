87546d15b4bf2902c662b96f48f85f22d3f1fed0
Charmy <desai.cha@husky.neu.edu>
2018-11-21 16:34:55 -0500

    POJO classes added for the final project

---
+++ ISFinalProject/src/Business/Role/SystemAdminRole.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+/**
+ *
+ * @author Charmy
+ */
+public class SystemAdminRole {
+
+    public SystemAdminRole() {
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
